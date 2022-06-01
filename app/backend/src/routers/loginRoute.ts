fa83d7a12005047b5c7ed25ff18905fdcf4088a8
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-06-01 16:49:14 -0300

    Feat LoginValidate route

--- app/backend/src/routers/loginRoute.ts
+++ app/backend/src/routers/loginRoute.ts
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import LoginController from '../controller/Login';
+import LoginValidateController from '../controller/LoginValidate';
 import validator from '../middlewares/Validator';
 import * as Schema from '../schemas';
 
 const loginRoute = Router();
 const login = new LoginController();
+const loginValidate = new LoginValidateController();
 
 loginRoute.route('/')
   .post(validator(Schema.body.login), login.execute);
+
+loginRoute.route('/validate')
+  .get(Schema.auth, loginValidate.execute);
 
 export default loginRoute;
