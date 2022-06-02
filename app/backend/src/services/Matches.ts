afe934b3c173eaeb68dc955914672b0c9cb3599b
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-06-02 12:25:10 -0300

    Refactored: include team informations in matches service return

--- app/backend/src/services/Matches.ts
+++ app/backend/src/services/Matches.ts
@@ -1,12 +1,16 @@
 import { StatusCodes } from 'http-status-codes';
-import ITeam from '../interfaces/ITeam';
+import MatchModel from '../database/models/Match';
 import TeamModel from '../database/models/Team';
 import RequestError from '../helper/RequestError';
 
 class MatchesService {
   public get = async () => {
-    const matchList = await TeamModel.findAll() as ITeam[];
-    if (!matchList) throw new RequestError(StatusCodes.UNAUTHORIZED, 'Does not have exist teams');
+    const matchList = await MatchModel.findAll({
+      include: [{ model: TeamModel, as: 'teamHome', attributes: ['teamName'] },
+        { model: TeamModel, as: 'teamAway', attributes: ['teamName'] }],
+    });
+
+    if (!matchList) throw new RequestError(StatusCodes.UNAUTHORIZED, 'Not have matches registered');
     return matchList;
   };
 }
