dd22bf3a3d636ec29f33a38f276e94eded81a8a8
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-06-02 12:27:38 -0300

    Feat: IMatchTeam interface

--- app/backend/src/interfaces/IMatch.ts
+++ app/backend/src/interfaces/IMatch.ts
@@ -1,4 +1,4 @@
-interface Team {
+interface ITeam {
   id?:number,
   homeTeam: number,
   homeTeamGoals: number,
@@ -7,4 +7,15 @@ interface Team {
   inProgress: boolean,
 }
 
-export default Team;
+interface IMatchTeam {
+  id?:number,
+  homeTeam: number,
+  homeTeamGoals: number,
+  awayTeam: number,
+  awayTeamGoals: number,
+  inProgress: boolean,
+  teamHome: object,
+  teamAway: object,
+}
+
+export { ITeam, IMatchTeam };
