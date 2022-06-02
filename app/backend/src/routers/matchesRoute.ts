15d42b5c176e0e7d59739d529e39e3c7c9620244
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-06-02 16:33:47 -0300

    Feat: includes match post route

--- app/backend/src/routers/matchesRoute.ts
+++ app/backend/src/routers/matchesRoute.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import MatchesController from '../controller/Matches';
+import Match from '../controller/CreateMatch';
 
 const matchesRoute = Router();
 const matches = new MatchesController();
+const match = new Match();
 
 matchesRoute.route('/')
-  .get(matches.get);
+  .get(matches.get)
+  .post(match.create);
 
 // matchesRoute.route('/:id')
 //   .get(matches.get);
