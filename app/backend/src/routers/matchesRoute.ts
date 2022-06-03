cc848058dfd3cbb8cf55a47933048123175244d9
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-06-03 16:16:53 -0300

    Feat: UpdateMatch routes

--- app/backend/src/routers/matchesRoute.ts
+++ app/backend/src/routers/matchesRoute.ts
@@ -1,19 +1,25 @@
 import { Router } from 'express';
 import MatchesController from '../controller/Matches';
 import NewMatch from '../controller/CreateMatch';
-import ChangeMatch from '../controller/FinishedMatch';
+import FinishedMatch from '../controller/FinishedMatch';
+import UpdatedMatch from '../controller/UpdateMatch';
 import * as Schema from '../schemas';
 
 const matchesRoute = Router();
 const matches = new MatchesController();
 const newMatch = new NewMatch();
-const changeMatch = new ChangeMatch();
+const updateMatch = new UpdatedMatch();
+const finishedMatch = new FinishedMatch();
 
 matchesRoute.route('/')
   .get(matches.get)
   .post(Schema.auth, newMatch.create);
 
+matchesRoute.route('/:id')
+  .patch(updateMatch.update)
+  .patch(finishedMatch.update);
+
 matchesRoute.route('/:id/finish')
-  .patch(changeMatch.update);
+  .patch(finishedMatch.update);
 
 export default matchesRoute;
