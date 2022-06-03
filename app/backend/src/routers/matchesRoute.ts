9fecc97b48b97536257ac1cb9b38f2a2a7ca1835
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-06-03 11:37:02 -0300

    Refactored: matchesRoute.

--- app/backend/src/routers/matchesRoute.ts
+++ app/backend/src/routers/matchesRoute.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import MatchesController from '../controller/Matches';
-// import Match from '../controller/CreateMatch';
+import Match from '../controller/CreateMatch';
+import * as Schema from '../schemas';
 
 const matchesRoute = Router();
 const matches = new MatchesController();
-// const match = new Match();
+const match = new Match();
 
 matchesRoute.route('/')
-  .get(matches.get);
-// .post(match.create);
+  .get(matches.get)
+  .post(Schema.auth, match.create);
 
 // matchesRoute.route('/:id')
 //   .get(matches.get);
