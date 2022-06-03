dcaabbf6af9c56a34a5ee448a39783cef43755e4
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-06-03 15:45:08 -0300

    Feat: Implement more filter and conditions in CreateMatch
    One to check if the team already exists, if homTeam and awayTeam are different and conditionated the create function to ignore problems cases.

--- app/backend/src/services/CreateMatch.ts
+++ app/backend/src/services/CreateMatch.ts
@@ -2,28 +2,26 @@ import { StatusCodes } from 'http-status-codes';
 import { IMatch } from '../interfaces/IMatch';
 import MatchModel from '../database/models/Match';
 import RequestError from '../helper/RequestError';
+import TeamModel from '../database/models/Team';
+import ITeam from '../interfaces/ITeam';
 
 class CreateMatchService {
   public create = async (body: IMatch, token: string) => {
-    const {
-      homeTeam,
-      awayTeam,
-      homeTeamGoals,
-      awayTeamGoals,
-    } = body;
-
     if (!token) throw new RequestError(StatusCodes.UNAUTHORIZED, 'Token is not validate');
-    // const matchListed = await MatchModel.findOne({ where: { homeTeam, awayTeam, inProgress } });
+    const { homeTeam, awayTeam } = body;
 
-    // if (matchListed) throw new RequestError(StatusCodes.UNAUTHORIZED, 'Match already exists');
+    const teamList = await TeamModel.findAll() as ITeam[];
+    const existsHomeTeam = teamList.find((team) => Number(team.id) === Number(homeTeam));
+    const existsAwayTeam = teamList.find((team) => Number(team.id) === Number(awayTeam));
+    if (!existsHomeTeam || !existsAwayTeam) {
+      throw new RequestError(StatusCodes.UNAUTHORIZED, 'There is no team with such id!');
+    }
+    if (homeTeam === awayTeam) {
+      const message = 'It is not possible to create a match with two equal teams';
+      throw new RequestError(StatusCodes.UNAUTHORIZED, message);
+    }
 
-    const newMatch = await MatchModel.create({
-      homeTeam,
-      awayTeam,
-      homeTeamGoals,
-      awayTeamGoals,
-      inProgress: true,
-    });
+    const newMatch = await MatchModel.create({ ...body, inProgress: true });
 
     return newMatch;
   };
