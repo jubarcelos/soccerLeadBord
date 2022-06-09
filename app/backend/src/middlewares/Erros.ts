360670137c46014327d4c0f73099eafd97d010d5
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-06-09 13:12:20 -0300

    Feat: login/validate errors

--- app/backend/src/middlewares/Erros.ts
+++ app/backend/src/middlewares/Erros.ts
@@ -1,10 +1,21 @@
-import * as Joi from 'joi';
+import { isError } from 'joi';
 import { NextFunction, Response, Request } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import { isCelebrateError } from 'celebrate';
+import { JsonWebTokenError } from 'jsonwebtoken';
 import RequestError from '../helper/RequestError';
 
 const handlerError = (err:Error, req:Request, res:Response, _next:NextFunction) => {
-  if (Joi.isError(err)) {
+  if (isCelebrateError(err)) {
+    return res.status(StatusCodes.UNAUTHORIZED)
+      .json({ message: err.details.entries().next().value[1].details[0].message });
+  }
+  // confiei no caminho que o Guilherme Augusto me indicou.
+  if (err instanceof JsonWebTokenError) {
+    return res.status(StatusCodes.UNAUTHORIZED)
+      .json({ message: 'Token is invalid' });
+  }
+  if (isError(err)) {
     return res.status(StatusCodes.BAD_REQUEST).json({ message: 'All fields must be filled' });
   }
   if (err instanceof RequestError) {
