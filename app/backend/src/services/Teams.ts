d220dddd52a53ef37af648ecd19093b5a34a8ee1
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-06-09 11:43:05 -0300

    Refractor: Remove inexistent conditional.

--- app/backend/src/services/Teams.ts
+++ app/backend/src/services/Teams.ts
@@ -1,12 +1,9 @@
-import { StatusCodes } from 'http-status-codes';
 import ITeam from '../interfaces/ITeam';
 import TeamModel from '../database/models/Team';
-import RequestError from '../helper/RequestError';
 
 class TeamsService {
   public get = async () => {
     const teamList = await TeamModel.findAll() as ITeam[];
-    if (!teamList) throw new RequestError(StatusCodes.UNAUTHORIZED, 'Does not have exist teams');
     return teamList;
   };
 }
