6ec56bc95a42e7322c89cc622d992527ae30f745
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-06-09 11:41:23 -0300

    Feat: TeamMock

---
+++ app/backend/src/tests/mocks/Team.ts
@@ -0,0 +1,22 @@
+import ITeam from "../../interfaces/ITeam";
+
+const Team: ITeam[] = [
+  {
+    id: 1,
+    teamName: "Avaí/Kindermann"
+},
+{
+    id: 2,
+    teamName: "Bahia"
+},
+{
+    id: 3,
+    teamName: "Botafogo"
+},
+{
+    id: 4,
+    teamName: "Corinthians"
+}
+]
+
+export default Team;
