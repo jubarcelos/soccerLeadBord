885f590d526660dc9badcf9565d98d24c33f8022
Julia Barcelos <juliabarcelos@Leandros-MacBook-Pro-2.local>
2022-06-14 21:04:35 -0300

    Feat: Match Mock

---
+++ app/backend/src/tests/mocks/Match.ts
@@ -0,0 +1,46 @@
+const Match = [
+  {
+  "id": 1,
+  "homeTeam": 16,
+  "awayTeam": 8,
+  "homeTeamGoals": 1,
+  "awayTeamGoals": 1,
+  "inProgress": false,
+  "teamHome": {
+      "teamName": "São Paulo"
+  },
+  "teamAway": {
+      "teamName": "Grêmio"
+  }
+},{
+  "id": 2,
+  "homeTeam": 9,
+  "awayTeam": 14,
+  "homeTeamGoals": 1,
+  "awayTeamGoals": 1,
+  "inProgress": false,
+  "teamHome": {
+      "teamName": "Internacional"
+  },
+  "teamAway": {
+      "teamName": "Santos"
+  }
+},
+{
+  "id": 3,
+  "homeTeam": 4,
+  "awayTeam": 11,
+  "homeTeamGoals": 3,
+  "awayTeamGoals": 0,
+  "inProgress": false,
+  "teamHome": {
+      "teamName": "Corinthians"
+  },
+  "teamAway": {
+      "teamName": "Napoli-SC"
+  }
+}];
+
+
+
+export { Match };
